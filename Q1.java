61bb5caf89dd061fedfce1af095c22d8ed18ff23
kennykenny789 <jtkennyg@gmail.com>
2021-01-10 15:15:49 +0800

    add Q1

---
+++ Q1.java
@@ -0,0 +1,16 @@
+package Homework1;
+
+
+// Q1:請設計一隻Java程式,計算12,6這兩個數值的和與積
+
+public class Q1 {
+	
+	public static void main(String[] args){
+		int a = 12;
+		int b = 6;
+	
+		System.out.println(a + b);
+		System.out.println(a * b);
+	}
+	
+}
